417471818ac778baa7b37cd48a955a9071712e2b
Giampaolo Rodola <g.rodola@gmail.com>
2018-08-14 00:42:42 +0200

    move keys definition in the h file

--- psutil/_psutil_osx.c
+++ psutil/_psutil_osx.c
@@ -903,116 +903,13 @@ psutil_boot_time(PyObject *self, PyObject *args) {
  */
 static PyObject *
 psutil_sensors_temperatures(PyObject *self, PyObject *args) {
-    PyObject *py_retlist = PyList_New(0);
-    PyObject *py_tuple = NULL;
     float temp;
-    smc_key smc_keys[] = {
-        // --- CPU
-        {"CPU", "TCXC", "PECI CPU"},
-        {"CPU", "TCXc", "PECI CPU"},
-        {"CPU", "TC0P", "CPU 1 Proximity"},
-        {"CPU", "TC0H", "CPU 1 Heatsink"},
-        {"CPU", "TC0D", "CPU 1 Package"},
-        {"CPU", "TC0E", "CPU 1"},
-        {"CPU", "TC1C", "CPU Core 1"},
-        {"CPU", "TC2C", "CPU Core 2"},
-        {"CPU", "TC3C", "CPU Core 3"},
-        {"CPU", "TC4C", "CPU Core 4"},
-        {"CPU", "TC5C", "CPU Core 5"},
-        {"CPU", "TC6C", "CPU Core 6"},
-        {"CPU", "TC7C", "CPU Core 7"},
-        {"CPU", "TC8C", "CPU Core 8"},
-        {"CPU", "TCAH", "CPU 1 Heatsink Alt."},
-        {"CPU", "TCAD", "CPU 1 Package Alt."},
-        {"CPU", "TC1P", "CPU 2 Proximity"},
-        {"CPU", "TC1H", "CPU 2 Heatsink"},
-        {"CPU", "TC1D", "CPU 2 Package"},
-        {"CPU", "TC1E", "CPU 2"},
-        {"CPU", "TCBH", "CPU 2 Heatsink Alt."},
-        {"CPU", "TCBD", "CPU 2 Package Alt."},
-        {"CPU", "TCSC", "PECI SA"},
-        {"CPU", "TCSc", "PECI SA"},
-        {"CPU", "TCSA", "PECI SA"},
-
-        // --- GPU
-        {"GPU", "TCGC", "PECI GPU"},
-        {"GPU", "TCGc", "PECI GPU"},
-        {"GPU", "TG0P", "GPU Proximity"},
-        {"GPU", "TG0D", "GPU Die"},
-        {"GPU", "TG1D", "GPU Die"},
-        {"GPU", "TG0H", "GPU Heatsink"},
-        {"GPU", "TG1H", "GPU Heatsink"},
-
-        // --- Memory
-        {"Memory", "Ts0S", "Memory Proximity"},
-        {"Memory", "TM0P", "Mem Bank A1"},
-        {"Memory", "TM1P", "Mem Bank A2"},
-        {"Memory", "TM8P", "Mem Bank B1"},
-        {"Memory", "TM9P", "Mem Bank B2"},
-        {"Memory", "TM0S", "Mem Module A1"},
-        {"Memory", "TM1S", "Mem Module A2"},
-        {"Memory", "TM8S", "Mem Module B1"},
-        {"Memory", "TM9S", "Mem Module B2"},
-
-        // --- HDD
-        {"HDD", "TH0P", "HDD Bay 1"},
-        {"HDD", "TH1P", "HDD Bay 2"},
-        {"HDD", "TH2P", "HDD Bay 3"},
-        {"HDD", "TH3P", "HDD Bay 4"},
-
-        // --- Battery
-        {"Battery", "TB0T", "Battery TS_MAX"},
-        {"Battery", "TB1T", "Battery 1"},
-        {"Battery", "TB2T", "Battery 2"},
-        {"Battery", "TB3T", "Battery"},
-
-        // --- Others
-        {"Others", "TN0D", "Northbridge Die"},
-        {"Others", "TN0P", "Northbridge Proximity 1"},
-        {"Others", "TN1P", "Northbridge Proximity 2"},
-        {"Others", "TN0C", "MCH Die"},
-        {"Others", "TN0H", "MCH Heatsink"},
-        {"Others", "TP0D", "PCH Die"},
-        {"Others", "TPCD", "PCH Die"},
-        {"Others", "TP0P", "PCH Proximity"},
-
-        {"Others", "TA0P", "Airflow 1"},
-        {"Others", "TA1P", "Airflow 2"},
-        {"Others", "Th0H", "Heatpipe 1"},
-        {"Others", "Th1H", "Heatpipe 2"},
-        {"Others", "Th2H", "Heatpipe 3"},
-
-        {"Others", "Tm0P", "Mainboard Proximity"},
-        {"Others", "Tp0P", "Powerboard Proximity"},
-        {"Others", "Ts0P", "Palm Rest"},
-        {"Others", "Tb0P", "BLC Proximity"},
-
-        {"Others", "TL0P", "LCD Proximity"},
-        {"Others", "TW0P", "Airport Proximity"},
-        {"Others", "TO0P", "Optical Drive"},
-
-        {"Others", "Tp0P", "Power Supply 1"},
-        {"Others", "Tp0C", "Power Supply 1 Alt."},
-        {"Others", "Tp1P", "Power Supply 2"},
-        {"Others", "Tp1C", "Power Supply 2 Alt."},
-        {"Others", "Tp2P", "Power Supply 3"},
-        {"Others", "Tp3P", "Power Supply 4"},
-        {"Others", "Tp4P", "Power Supply 5"},
-        {"Others", "Tp5P", "Power Supply 6"},
-
-        {"Others", "TS0C", "Expansion Slots"},
-        {"Others", "TA0S", "PCI Slot 1 Pos 1"},
-        {"Others", "TA1S", "PCI Slot 1 Pos 2"},
-        {"Others", "TA2S", "PCI Slot 2 Pos 1"},
-        {"Others", "TA3S", "PCI Slot 2 Pos 2"},
-
-        {"", ""} // Marker for end of array
-    };
+    smc_key* iter_key = smc_keys;
+    PyObject *py_tuple = NULL;
+    PyObject *py_retlist = PyList_New(0);
 
     if (py_retlist == NULL)
         return NULL;
-
-    smc_key* iter_key = smc_keys;
     while (iter_key->key[0] != '\0') {
         temp = SMCGetTemperature(iter_key->key);
         if (1) {  // TODO: remove this
